c2fbe99f0b2b99b711a7171bb7537ec22286fbac
abbos91 <abbos.shomurodov91@gmail.com>
2022-03-19 17:54:36 +0900

    button style changed

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const mainSection = document.querySelector(".main");
 const thankYouSection = document.querySelector(".thank-you-section");
 const rateNum = document.querySelector(".rate-num");
 
+submitBtn.addEventListener("mouseover", () => {
+  if (rateNum === 0) {
+    btn.style.cursor = "not-allowed";
+  } else {
+    btn.style.cursor = "pointer";
+  }
+});
+
 stars.forEach(function (star, indx) {
   star.addEventListener("click", function () {
     submitBtn.addEventListener("click", function () {
