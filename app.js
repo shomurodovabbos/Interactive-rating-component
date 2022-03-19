3ceba21992f46ebee23e733347c214b680a18419
abbos91 <abbos.shomurodov91@gmail.com>
2022-03-19 16:58:48 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const submitBtn = document.querySelector(".submit-btn");
+const stars = document.querySelectorAll(".rating-nums li a");
+const mainSection = document.querySelector(".main");
+const thankYouSection = document.querySelector(".thank-you-section");
+const rateNum = document.querySelector(".rate-num");
+
+stars.forEach(function (star, indx) {
+  star.addEventListener("click", function () {
+    submitBtn.addEventListener("click", function () {
+      mainSection.classList.add("hidden");
+      thankYouSection.classList.remove("hidden");
+      rateNum.innerText = `${indx + 1}`;
+    });
+  });
+});
